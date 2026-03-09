954a085031c44653d79945b7cd001db4d555c5fd
Shreyash Patel <shreyash.patel@prutech.com>
2026-03-09 13:29:15 +0530

    Fix DB error

--- database.py
+++ database.py
@@ -1,8 +1,11 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
+# Use /tmp/todos.db as it's typically writable even in read-only container file systems
+DB_PATH = os.getenv("DB_PATH", "/tmp/todos.db")
+SQLALCHEMY_DATABASE_URL = f"sqlite:///{DB_PATH}"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
